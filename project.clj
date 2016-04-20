79a63fbd2078c5bdd8709305e56e9da6fa19a465
Jason Bell <jason@mastodonc.com>
2016-04-20 14:41:17 +0100

    Functions to pull daily met office data based on siteid.

--- project.clj
+++ project.clj
@@ -5,6 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :target-path "target/%s"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.csv      "0.1.2"]
+                 [clj-http                  "2.0.0"]
+                 [clj-time                  "0.10.0"]
+                 [org.clojure/data.json     "0.2.6"]
+                 [org.clojure/tools.cli     "0.3.3"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.async "0.2.371"]
                  [com.stuartsierra/component "0.3.1"]
                  [org.onyxplatform/onyx "0.8.11"]
